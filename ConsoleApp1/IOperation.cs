5c2ae518cb8633e9f8946936c4dbeac772498484
Alva <asjun2lin@outlook.com>
2022-01-14 15:48:51 +0800

    add 05. 在 .NET 中用依赖注入

---
+++ ConsoleApp1/IOperation.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1;
+
+public interface IOperation
+{
+    string OperationId { get; }
+}
+
+public interface ITransientOperation : IOperation
+{
+}
+
+public interface IScopedOperation : IOperation
+{
+}
+
+public interface ISingletonOperation : IOperation
+{
+}
+
+public class DefaultOperation
+    : ITransientOperation,
+        IScopedOperation,
+        ISingletonOperation
+{
+    // 随机一个 Guid，取最后 4 个字符
+    public string OperationId { get; } = Guid.NewGuid().ToString()[^4..];
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp1;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+   .ConfigureServices(services =>
+    {
+        services.AddSingleton<OperationLogger>()
+           .AddTransient<ITransientOperation, DefaultOperation>()
+           .AddScoped<IScopedOperation, DefaultOperation>()
+           .AddSingleton<ISingletonOperation, DefaultOperation>();
+    })
+   .Build();
+
+ExemplifyScoping(host.Services, "Scope 1");
+ExemplifyScoping(host.Services, "Scope 2");
+
+await host.RunAsync();
+
+
+static void ExemplifyScoping(IServiceProvider services, string scope)
+{
+    using IServiceScope serviceScope = services.CreateScope();
+    IServiceProvider    provider     = serviceScope.ServiceProvider;
+
+    var logger = provider.GetRequiredService<OperationLogger>();
+    logger.LogOperations($"{scope}: Call 1 ...");
+    Console.WriteLine();
+
+    logger = provider.GetRequiredService<OperationLogger>();
+    logger.LogOperations($"{scope}: Call 2 ...");
+    Console.WriteLine();
+}
