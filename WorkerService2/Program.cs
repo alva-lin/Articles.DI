d196648e93f90b8a711876020e125d70ebc8ca09
Alva <asjun2lin@outlook.com>
2022-01-14 14:43:59 +0800

    update 02. 解耦依赖，使用依赖注入

--- WorkerService2/Program.cs
+++ WorkerService2/Program.cs
@@ -4,6 +4,8 @@ IHost host = Host.CreateDefaultBuilder(args)
    .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
+        // 注册服务
+        services.AddSingleton<IMessageWriter, MessageWriter>();
     })
    .Build();
 
--- WorkerService2/Worker.cs
+++ WorkerService2/Worker.cs
@@ -2,12 +2,14 @@ namespace WorkerService2;
 
 public class Worker : BackgroundService
 {
-    private readonly ILogger<Worker> _logger;
+    // 松耦合，只依赖接口，不依赖具体实现。Worker 类不负责初始化成员
+    private readonly IMessageWriter _messageWriter;
 
 
-    public Worker(ILogger<Worker> logger)
+    // 从构造函数传递注入具体的实例，将其赋值给 _messageWriter 成员
+    public Worker(IMessageWriter messageWriter)
     {
-        _logger = logger;
+        _messageWriter = messageWriter;
     }
 
 
@@ -15,8 +17,21 @@ public class Worker : BackgroundService
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            _messageWriter.Write($"Worker running at: {DateTimeOffset.Now}");
             await Task.Delay(1000, stoppingToken);
         }
+    }
+}
+
+public interface IMessageWriter
+{
+    void Write(string message);
+}
+
+public class MessageWriter : IMessageWriter
+{
+    public void Write(string message)
+    {
+        Console.WriteLine($"MessageWriter.Write(message: \"{message}\")");
     }
 }
