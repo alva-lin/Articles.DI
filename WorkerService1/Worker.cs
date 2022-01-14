92f7475651ca879a6b12ff572d7320732e0557bd
Alva <asjun2lin@outlook.com>
2022-01-14 14:36:19 +0800

    update 01. 硬编码依赖，需要自行初始化成员变量

--- WorkerService1/Worker.cs
+++ WorkerService1/Worker.cs
@@ -2,21 +2,24 @@
 
 public class Worker : BackgroundService
 {
-    private readonly ILogger<Worker> _logger;
-
-
-    public Worker(ILogger<Worker> logger)
-    {
-        _logger = logger;
-    }
+    // 硬编码依赖，需要自行初始化成员变量
+    private readonly MessageWriter _messageWriter = new();
 
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            _messageWriter.Write($"Worker running at: {DateTimeOffset.Now}");
             await Task.Delay(1000, stoppingToken);
         }
+    }
+}
+
+public class MessageWriter
+{
+    public void Write(string message)
+    {
+        Console.WriteLine($"MessageWriter.Write(message: \"{message}\")");
     }
 }
